300f4ad5d88f6036b566961d2397e836b07687b5
YuvaKiranB <yuvakiranbandaru@gmail.com>
2024-01-30 09:29:41 +0000

    code

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  <div className="main">
+    <h1 className="h1">Congratulations</h1>
+    <div className="card">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+      />
+      <h2 className="h2">Kiran V</h2>
+      <p className="p1">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+      />
+    </div>
+  </div> // Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
+s;
